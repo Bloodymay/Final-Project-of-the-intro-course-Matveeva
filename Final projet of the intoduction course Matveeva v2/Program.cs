654e9e57780bb3448170cf28ee24734df723ace7
bmay <bloodymay456@gmail.com>
2023-01-25 23:14:06 +0300

    FinalProject2 added

---
+++ Final projet of the intoduction course Matveeva v2/Program.cs
@@ -0,0 +1,53 @@
+﻿int number = ReadData("Сколько строк вы хотите ввести?"); 
+string[] arr = GenArray(number); 
+PrintData("Сгенерированный массив из введенных строк: ", arr); 
+ 
+ 
+//Метод, считывает длину массива, введенную пользователем 
+int ReadData(string msg) 
+{ 
+    Console.WriteLine(msg); 
+    return int.Parse(Console.ReadLine() ?? "0"); 
+} 
+ 
+//Метод, считывает строки, введенные пользователем 
+string ReadStrings(string msg) 
+{ 
+    Console.WriteLine(msg); 
+    return (Console.ReadLine() ?? "0"); 
+} 
+ 
+// Метод генерирует массив из введенных пользователем строк 
+string[] GenArray(int num) 
+{ 
+    string[] arr = new string[num]; 
+    for (int i = 0; i < num; i++) 
+    { 
+        arr[i] = ReadStrings("Введите данные: "); 
+    } 
+    return arr; 
+} 
+ 
+// Метод выводит данные пользователю 
+void PrintData(string res, string[] arr) 
+{ 
+    Console.WriteLine(res); 
+    PrintStringArray(arr); 
+} 
+ 
+// Метод печатает одномерный массив 
+void PrintStringArray(string[] arr) 
+{ 
+    Console.Write("["); 
+    for (int i = 0; i < arr.Length-1 ; i++) 
+    { 
+        if (arr[i].Length <= 3) 
+        { 
+            Console.Write(arr[i] + ", "); 
+            
+        } 
+        
+    } 
+    
+    Console.Write( "]"); 
+}
